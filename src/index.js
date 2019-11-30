b0cbaa74f355465261c7123f77573ff5c45d23ea
Muhammed Mirza Kılıç <muhammedmirzakilic@gmail.com>
2019-12-01 00:02:44 +0300

    last menus design changed

--- src/index.js
+++ src/index.js
@@ -2,4 +2,3 @@ import "./index.css";
 import "./assets/hamburger.png";
 import "./assets/promotion-menu-burger.png";
 import "./assets/promotion-menu-burger1.png";
-import "./assets/sushi-background.png";
